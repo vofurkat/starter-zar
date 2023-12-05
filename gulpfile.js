e315ea29407c63525fd0110feea568b5efbfa781
Furkat <vfurkat@gmail.com>
2023-12-05 12:52:18 +0500

    My Starter

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,44 @@
-const {src, dest, watch, series, parallel} = require("gulp");
-const gulpFileInclude = require("gulp-file-include");
-const browserSync = require("browser-sync").create();
+import gulp from "gulp";
+import browserSync from "browser-sync";
 
-const html = () =>{
-    return src("./app/html/*.html")
-    .pipe(gulpFileInclude())
-    .pipe(dest("./dist"))
-    .pipe(browserSync.stream());
-}
+// include url
+import url from "./config/url.js";
+
+// include tasks
+import clear from "./tasks/clear.js";
+import pug from "./tasks/pug.js";
+import scss from "./tasks/scss.js";
+import image from "./tasks/image.js";
+import font from "./tasks/font.js";
+import js from "./tasks/script.js";
 
 const watching = () => {
-    watch("./app/html/*.html", html)
+    gulp.watch(url.pug.watch, pug).on('all', browserSync.reload);
+    gulp.watch(url.scss.watch, scss).on('all', browserSync.reload);
+    gulp.watch(url.image.watch, image).on('all', browserSync.reload);
+    gulp.watch(url.font.watch, font).on('all', browserSync.reload);
+    gulp.watch(url.js.watch, js).on('all', browserSync.reload);
 }
 
 const server = () =>{
     browserSync.init({
         server: {
-            baseDir: "./dist"
+            baseDir: url.pro
         }
     })
 }
 
-exports.server = server;
-exports.watch = watching;
-exports.html = html;
-
-exports.start = series(
-    html,
-    parallel(watching,server)
+const start = gulp.series(
+    clear,
+    gulp.parallel(pug, scss, image, font, js),
+    gulp.parallel(watching,server)
 );
+
+const finish = gulp.series(
+    clear,
+    gulp.parallel(pug, scss, image, font, js)
+);
+
+export default url.proIs
+    ? finish
+    : start;
